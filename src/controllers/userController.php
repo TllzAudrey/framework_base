dea1bc3039824b5b281b1631cc643b76dd61a380
Audrey <audrey.telliez@estiam.com>
2024-05-14 13:14:13 +0200

    gestion du systeme de modèle (45%)

--- src/controllers/userController.php
+++ src/controllers/userController.php
@@ -1,11 +1,33 @@
 <?php
-
+#[AllowDynamicProperties]
 class UserController{
+
     public function __construct(){
-        echo "user controller";
+        $this->loadModel("user");
     }
+    private function loadModel($mdl_name){
+        $file_name = $mdl_name."Model";
+        if(file_exists(MODELS.DS.$file_name.".php")){
+            require_once(MODELS.DS.$file_name.".php");
+        }else{
+            echo "Le modèle n'éxiste pas.";
+        }
+        $this->$mdl_name = new $file_name();
+    }
+
     public function index($id_user){
-        echo $id_user;
+        var_dump($this);
+        $this->user->insert([
+            "nom" => "Telliez",
+            "prenom" => "Dominique",
+            "email" => "dom@estiam.com",
+            "password" => "estiam1234"
+
+        ]);
+        /*$this->user->find([
+            "id" => $id_user,
+            "nom" => "Telliez"
+        ]);*/
     }
 
 }
--- src/models/userModel.php
+++ src/models/userModel.php
@@ -1,11 +1,80 @@
 <?php
 
 class UserModel{
+    private $table = "user";
+    private $dbh = null;
+    private $stmt;
+    private $sql;
+    private $params ;
+    
     public function __construct(){
-        echo "user controller";
+        $user = "root";
+        $pass = "";
+        try {
+            $this->dbh = new PDO('mysql:host=localhost;dbname=framework_base', $user, $pass);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        //var_dump($this->dbh);
     }
-    public function index($id_user){
-        echo $id_user;
+
+    
+    public function findAll(){
+        $this->sql = "SELECT * FROM ".$this->table;
+        $result = $this->fetchAll();
+        //var_dump($this->dbh);
+        var_dump($result);
+    }
+
+    public function find($cond){
+        $this->sql = "SELECT * FROM ".$this->table." WHERE ";
+        $this->params = $cond;
+
+        foreach($cond as $key => $value){
+            $this->sql .= $key." = :".$key;
+            if(count($cond)>1){
+                $this->sql .= " AND ";
+            }
+        }
+
+        $this->sql = substr($this->sql, 0, -4);
+        //$result = $this->fetchAll();
+        $result = $this->fetch();
+        
+        var_dump($result);
+    }
+
+    public function insert($data){
+        $this->sql = "INSERT INTO ".$this->table." (";
+        $this->params = $data;
+        $values = "";
+        foreach($data as $k => $v){
+            $this->sql .= $k.", ";
+            $values .= ":".$k.", ";
+        }
+        $this->sql = substr($this->sql, 0, -2);
+        $values = substr($values, 0, -2);
+        $this->sql .= ") VALUES(".$values.")";
+
+        $result = $this->fetch();
+        var_dump($result);
+    }
+
+    private function prepare(){
+        $this->stmt = $this->dbh->prepare($this->sql);
+    }
+    private function execute(){
+        $this->stmt->execute($this->params);
+    }
+    private function fetchAll($mode = PDO::FETCH_ASSOC){
+        $this->prepare($this->sql);
+        $this->execute();
+        return $this->stmt->fetchAll($mode); // PDO::FETCH_ASSOC pour voir le nom des colones
+    }
+    private function fetch($mode = PDO::FETCH_ASSOC){
+        $this->prepare($this->sql);
+        $this->execute();
+        return $this->stmt->fetch($mode); // PDO::FETCH_ASSOC pour voir le nom des colones
     }
 
 }
