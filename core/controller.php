c4072dca3ea63debac02fc820fc054679ec530e0
Audrey <audrey.telliez@estiam.com>
2024-05-16 15:43:37 +0200

    optimisation systhème controller

---
+++ core/controller.php
@@ -0,0 +1,23 @@
+<?php
+#[AllowDynamicProperties]
+class Controller{
+    public function __construct(){
+        $this->loadModel("role");
+        if(method_exists($this, "__init")){
+            $this->__init();
+        }
+        $ctrl_name = str_replace("Controller","",get_class($this));
+        echo $ctrl_name;
+    }
+    private function loadModel($mdl_name){
+        
+        $file_name = $mdl_name."Model";
+        if(file_exists(MODELS.DS.$file_name.".php")){
+            require_once(MODELS.DS.$file_name.".php");
+        }else{
+            echo "Le modèle n'éxiste pas.";
+        }
+        $this->$mdl_name = new $file_name();
+    }
+
+}
